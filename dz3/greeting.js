c1af419a3ee93e04879e0abf8bbe9f72ee1b2af1
joma01 <106875236+joma01@users.noreply.github.com>
2022-06-14 22:28:49 +0600

    Add files via upload

---
+++ dz3/greeting.js
@@ -0,0 +1,17 @@
+function countChar(c) {
+    console.log(c.length)
+}
+countChar("text")
+
+function numbers (number1, number2){
+    if (number1 < number2){
+        console.log("Первое меньше")
+    }else if (number2 > number1){
+        console.log("Второе меньше")
+    }else {
+        console.log("Они равны")
+    }
+}
+
+
+
